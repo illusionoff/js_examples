d2fa94e00502d40a06c7f71b2eecfca4585343f7
illusionoff <venom12@inbox.ru>
2021-09-30 18:56:55 +0300

    jsdoc

---
+++ doc.js
@@ -0,0 +1,18 @@
+/**
+ * Returns the sum of a and b
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function sum(a, b) {
+  return a + b;
+}
+/**
+* Return the diff fo a and b
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+function diff(a, b) {
+  return a - b;
+}

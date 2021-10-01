8937807a39208c05b74c2903b3f5d5e8af845072
MiniPC <venom12@inbox.ru>
2021-10-01 15:58:03 +0300

    parseCSV() for reactcriptoarbitr_tets

--- app.js
+++ app.js
@@ -2090,3 +2090,31 @@ var util = require('util');
 
 // let variableClosure = closure();
 // let variableClosure2 = closure();
+/////////////////////////////////////////////////////////////
+// for reactcriptoarbitr_tets
+const fs = require("fs");
+const parse = require('csv-parse');
+
+function parseCSV() {
+  // fs.readFile("./csv/test_profit_12.csv", "utf8",
+  fs.readFile("./csv/test_profit_12_copy.csv", "utf8",
+    function (error, input) {
+      console.log("Асинхронное чтение файла");
+      if (error) throw error; // если возникла ошибка
+      // console.log('data file:', data);
+      parse(input, {
+        comment: '#',
+        // columns: ['col', 'bayGate', 'bayBith', 'sellGate', 'sellBith', 'diffSell', 'diffBay', 'timeServer', 'timeBith', 'init']
+      }, function (err, output) {
+        if (err) throw err; // если возникла ошибка
+        console.log('output=', output);
+        // assert.deepStrictEqual(
+        //   output,
+        //   [ [ '1', '2', '3', '4' ], [ 'a', 'b', 'c', 'd' ] ]
+        // )
+      })
+
+    });
+}
+
+parseCSV();

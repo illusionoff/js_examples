07edc2b2a147c3792195a9cb8622339ddfe483c0
MiniPC <venom12@inbox.ru>
2021-11-04 12:42:22 +0300

    React TestReferentialEqualityUseMemo 1.1

--- my-app/src/react/TestReferentialEqualityUseMemo.js
+++ my-app/src/react/TestReferentialEqualityUseMemo.js
@@ -1,6 +1,7 @@
-import React, { useState, useMemo,useEffect } from "react";
+import React, { useState, useMemo, useEffect, useCallback } from "react";
 
 function makeObject(a, b, c) {
+  console.log('function makeObject');
   return { a, b, c };
 }
 
@@ -19,10 +20,11 @@ function doSomething (myObject,a) {
     console.log('props=', {props})
   const [count, setCount] = useState(0);
   const myObject = useMemo(() => makeObject(a, b, c), [a, b, c]);
+  // const myObject = useCallback(() => makeObject(a, b, c), [a, b, c]);
 
   useEffect(() => {
-    doSomething(myObject,a);
-  }, [myObject,a]);
+    doSomething(myObject);
+  }, [myObject]);
 
   return (
     <>
@@ -32,11 +34,34 @@ function doSomething (myObject,a) {
         onClick={() => {
           setCount(count + 1);
           a = count;
-          props.setState({a:count,b:2,c:3})
+          // props.setState({a:count,b:2,c:3})
+          props.setState({a:1,b:2,c:3})
           console.log('a button=',a);
         }}
       >
         Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          a = count;
+          // props.setState({a:count,b:2,c:3})
+          props.setState({a:2,b:2,c:2})
+          // console.log('a button=',a);
+        }}
+      >
+        Increment2
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          a = count;
+          // props.setState({a:count,b:2,c:3})
+          props.setState({a:3,b:3,c:3})
+          // console.log('a button=',a);
+        }}
+      >
+        Increment3
       </button>
     </>
   );

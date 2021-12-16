a586777fb8f6c0cc9aacdd58159bb0f1a1d28021
MiniPC <venom12@inbox.ru>
2021-12-16 16:10:04 +0300

    Неопределенный набор параметров работает

--- app.js
+++ app.js
@@ -2582,3 +2582,20 @@ function fakeFetch(url, params = 1000) {
 
 // // console.log(getName("Alex", "Siniakou"));
 // console.log(getName("Alex"));
+
+// Неопределенный набор параметров
+
+function addNumbers(firstNumber, ...numberArray) {
+
+  let result = firstNumber;
+  for (let i = 0; i < numberArray.length; i++) {
+    result += numberArray[i];
+  }
+  return result;
+}
+
+let num1 = addNumbers(3, 7, 8);
+console.log(num1); // 18
+
+let num2 = addNumbers(3, 7, 8, 9, 4);
+console.log(num2); // 31
